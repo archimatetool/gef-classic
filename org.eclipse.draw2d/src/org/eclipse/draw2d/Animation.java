3f7946b64466355788a5d9ff62f128e1c378e01d
Phillipus <p.beauvoir@dadabeatnik.com>
2026-01-16 17:01:52 +0000

    Fix Animation not working

--- org.eclipse.draw2d/src/org/eclipse/draw2d/Animation.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/Animation.java
@@ -19,6 +19,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.swt.widgets.Display;
+
 /**
  * A utility for coordinating figure animations. During animation, multiple
  * <i>animators</i> are employed to capture the <em>initial</em> and
@@ -108,7 +110,11 @@ public class Animation {
 
 		while (progress != 0) {
 			step();
-			updateManager.performUpdate();
+
+			// Fix animation not working
+			Display.getDefault().readAndDispatch();	
+			// updateManager.performUpdate();
+			
 			if (progress == 1.0) {
 				progress = 0;
 			} else {
@@ -153,7 +159,7 @@ public class Animation {
 	}
 
 	/**
-	 * Returns the animation progress, where 0.0 &lt; progress &#8804; 1.0.
+	 * Returns the animation progress, where 0.0 < progress &#8804; 1.0.
 	 *
 	 * @return the progress of the animation
 	 * @since 3.2
@@ -251,7 +257,14 @@ public class Animation {
 	 * @since 3.2
 	 */
 	public static void run(int duration) {
-		if (state == 0 || state == PLAYBACK) {
+        // Because our fix calls Display.getDefault().readAndDispatch();
+        // run() can be called again whilst playing back
+        // This guards against that
+        if(state == PLAYBACK) {
+            return;
+        }
+
+		if (state == 0) {
 			return;
 		}
 		try {
